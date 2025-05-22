98310c34e17ac09590b4fa84063cfebfc95cad73
toby <tobygeneva@hotmail.com>
2025-05-22 14:00:20 +0200

    Initial Commit

---
+++ mg3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mg3
+//
+//  Created by Toby Simpson on 22.05.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
